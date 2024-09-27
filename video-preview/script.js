f4726b0381279d3c63d28540aa4da7066f748272
brisa16 <brisacardenas@liceovvh.cl>
2024-09-27 11:02:34 -0300

    ok

--- video-preview/script.js
+++ video-preview/script.js
@@ -3,13 +3,19 @@ console.log("page loaded...");
 function playVideo(secondaryVideo){
     video.play();
     var mainVideo = document.getElementById("mainVideo");
+    var videoTitle = secondaryVideo.parentElement.querySeletor(".video-title");
+    var principalTitle = document.getElementById("title-principal");
 
     var tempVideoSrc = mainVideo.src;
+    var temptitle = principalTitle.textContent;
     
     mainVideo.src = secondaryVideo.src;
     mainVideo.play();
 
     secondaryVideo.src = tempVideoSrc;
+
+    principalTitle.textContent = videoTitle;
+    secondaryVideo.parentElement.querySeletor(".video-title").textContent = temptitle;
 }
 
 function pauseVideo(secondaryVideo){
